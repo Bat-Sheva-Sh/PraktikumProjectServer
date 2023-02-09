606bbb140bb226f4a0806e37703bdea17cbbe3c7
Bat-Sheva-Sh <113825432+Bat-Sheva-Sh@users.noreply.github.com>
2023-02-10 01:56:11 +0200

    Update IChildRepository.cs

--- MyProject.Repositories/Interfaces/IChildRepository.cs
+++ MyProject.Repositories/Interfaces/IChildRepository.cs
@@ -11,7 +11,6 @@ namespace MyProject.Repositories.Interfaces
     public interface IChildRepository
     {
         Task<List<Child>> GetAllAsync();
-        //Task<Child> GetByIdAsync(int id);
         Task<Child> GetByTzAsync(string childId);
         Task<Child> AddAsync(string name, string childId, DateTime dateOfBirth, int parentId);
         Task<Child> UpdateAsync(int id, string name, string childId, DateTime dateOfBirth, int parentId);
